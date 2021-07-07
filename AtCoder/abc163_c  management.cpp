b15bd1f80f91076b7266816890abfda44476a76d
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2021-07-07 13:10:18 +0200

    ...

--- AtCoder/abc163_c  management.cpp
+++ AtCoder/abc163_c  management.cpp
@@ -6,10 +6,15 @@ void Fast_Code(){
 ios_base::sync_with_stdio(0);
 cin.tie(nullptr); cout.tie(nullptr);
 }
-
+int a [200000+5];
 int main(){ Fast_Code();
-
-
+    int n; cin >> n;
+    for(int i=1; i<n; ++i){
+        int in; cin>>in;
+        ++a[in];
+    }
+    for(int i=1; i<=n; i++)
+        cout << a[i] << endl;
     return (0);
 }
 
